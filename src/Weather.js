4de55f9af72c32438d8b5fe8f1facf98a3809004
Liridona <liridonaosm@gmail.com>
2021-06-07 19:29:14 +0200

    put in apikey and alert city

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,19 @@
 import react from "react";
 
-export default function Weather() {
-    return (
-        <h2>
-            hello from weather
-        </h2>
-    )
+import axios from "axios";
+
+export default function Weather(props) {
+
+function handleResponse(response) {
+ alert(`The weather in Oslo is ${response.data.main.temp}Celcius`);
 }
+
+let apiKey = "c898564bae75feba5b1af523cf4a1211";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(handleResponse);
+return(
+    <h2>Hello from Weather</h2>
+);
+}
+
